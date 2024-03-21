d20c051df56a8557a133422032be5005134cad23
Angel Pimentel <angelpimentelcontact@gmail.com>
2024-03-21 11:48:14 -0400

    Change file name

--- main/pos_commission/__manifest__.py
+++ main/pos_commission/__manifest__.py
@@ -29,7 +29,7 @@
     'data': [
         'security/ir.model.access.csv',
 
-        'views/pos_commission_wizard/views.xml',
-        'views/product_template/product_template_form.xml',
+        'views/pos_commission_wizard/pos_commission_wizard_form.xml',
+        'views/pos_commission_wizard/pos_commission_wizard_menuitem.xml',
     ],
 }
