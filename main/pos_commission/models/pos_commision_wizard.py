257eb3495d725c9096612183353f4fbd237eda28
Angel Pimentel <angelpimentelcontact@gmail.com>
2024-03-20 21:53:30 -0400

    Change default start date

--- main/pos_commission/models/pos_commision_wizard.py
+++ main/pos_commission/models/pos_commision_wizard.py
@@ -1,29 +1,12 @@
 from odoo import models, fields, api
+from datetime import datetime
 
 
 class PosCommissionWizard(models.Model):
     _name = 'pos.commission.wizard'
     _description = 'Point of Sale Commission Report'
 
-    def _default_start_date(self):
-        """ Find the earliest start_date of the latests sessions """
-        # restrict to configs available to the user
-        config_ids = self.env['pos.config'].search([]).ids
-        # exclude configs has not been opened for 2 days
-        self.env.cr.execute("""
-            SELECT
-            max(start_at) as start,
-            config_id
-            FROM pos_session
-            WHERE config_id = ANY(%s)
-            AND start_at > (NOW() - INTERVAL '2 DAYS')
-            GROUP BY config_id
-        """, (config_ids,))
-        latest_start_dates = [res['start'] for res in self.env.cr.dictfetchall()]
-        # earliest of the latest sessions
-        return latest_start_dates and min(latest_start_dates) or fields.Datetime.now()
-
-    start_date = fields.Date(required=True, default=_default_start_date)
+    start_date = fields.Date(required=True, default=datetime.today().replace(day=1))
     end_date = fields.Date(required=True, default=fields.Datetime.now)
 
     @api.onchange('start_date')
