f31ad4296cea155e825d271f9b8004e6d4dac1ce
Angel Pimentel <angelpimentelcontact@gmail.com>
2024-03-23 20:33:05 -0400

    Add commission limit

--- main/pos_commission/models/product_template.py
+++ main/pos_commission/models/product_template.py
@@ -1,7 +1,30 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    commission = fields.Float()
+    commission = fields.Float(digits=(3, 2))
+
+    @api.model
+    def create(self, vals):
+        self.verify_commission_value(vals)
+        return super().create(vals)
+
+    def write(self, vals):
+        self.verify_commission_value(vals)
+        return super().write(vals)
+
+    @api.onchange('commission')
+    def onchange_commission(self):
+        self.verify_commission_value()
+
+    def verify_commission_value(self, vals=None):
+        if vals is None:
+            vals = {}
+
+        commission = vals.get('commission', self.commission)
+
+        if commission > 100:
+            raise UserError(_("Commission percentage couldn't be greater than 100."))
